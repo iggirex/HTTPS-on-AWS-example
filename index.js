73a9fe35f9216bf96661883188cedd0c9adcdc39
Ubuntu <ubuntu@ip-172-31-24-114.us-west-2.compute.internal>
2018-01-03 23:57:58 +0000

    changed server to include https and keys, ignoring keys folder

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require('express')
-const app = express()
+const https = require("https");
+const express = require("express");
+const fs = require("fs");
 
-app.use(express.static("static"));
+const app = express();
+
+//app.use(express.static("static"));
+
+const options = {
+	cert: fs.readFileSync("./sslcert/fullchain.pem"),
+	key: fs.readFileSync("./sslcert/privkey.pem")
+};
 
 app.get("/", (req, res) => {
   res.send("yo it's yer boi IgMoney WADDDUUUPP!?!")
 })
-app.listen(3000, () => console.log("Server running on port 3000"))
+app.listen(80, () => console.log("Server running on port 80"));
+
+https.createServer(options, app).listen(8483);
