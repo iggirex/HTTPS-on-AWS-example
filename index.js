98bf8126ddda6dcc37bc63ffbf35f911f656aa78
Ubuntu <ubuntu@ip-172-31-24-114.us-west-2.compute.internal>
2018-01-04 20:33:14 +0000

    trouble shooting https

--- index.js
+++ index.js
@@ -4,16 +4,19 @@ const fs = require("fs");
 
 const app = express();
 
-//app.use(express.static("static"));
+app.use(express.static("static"));
 
 const options = {
 	cert: fs.readFileSync("./sslcert/fullchain.pem"),
 	key: fs.readFileSync("./sslcert/privkey.pem")
 };
 
+app.use(require("helmet")());
+
 app.get("/", (req, res) => {
   res.send("yo it's yer boi IgMoney WADDDUUUPP!?!")
 })
+
 app.listen(80, () => console.log("Server running on port 80"));
 
-https.createServer(options, app).listen(8483);
+https.createServer(options, app).listen(443);
